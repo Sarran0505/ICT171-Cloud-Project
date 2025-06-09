c0eac608a6b20132c3c9bea46a73787e9f71af89
thelatest <thelatest@kali>
2025-06-09 17:37:21 +0800

    fifth commit

--- check_ports.sh
+++ check_ports.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
-echo "Liste des ports ouverts sur le serveur :"
-sudo netstat -tuln
+
+echo "List of open ports on the server:"
+echo "- - - - - - - - - - - - - - - - - - - - - "
+sudo netstat -tulnp
+echo
+echo "- - - - - - - - - - - - - - - - - - - - - "
+echo "Checking for suspicious ports..."
+
+
+EXPECTED_PORTS="22 80 443"
+OPEN_PORTS=$(sudo netstat -tuln | grep LISTEN | awk '{print $4}' | sed 's/.*://')
+
+echo "Expected ports: $EXPECTED_PORTS"
+echo "Open ports    : $OPEN_PORTS"
+
+echo
+for port in $OPEN_PORTS; do
+  if [[ "$EXPECTED_PORTS" =~ "$port" ]]; then
+    echo "🎉 Port $port : OK (expected)"
+  else
+    echo "❌ Port $port : suspicious or unexpected!"
+  fi
+done
 
